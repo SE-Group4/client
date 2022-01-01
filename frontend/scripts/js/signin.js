b8d12d49a684f5189073aa8dd750e4c1631eda49
edgararthur <gilfoylearthur@protonmail.com>
2022-01-01 12:28:28 +0000

    script to retrieve signin info from the user

---
+++ frontend/scripts/js/signin.js
@@ -0,0 +1,7 @@
+const signinForm = document.querySelector('.card form');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formData = Object.fromEntries(new FormData(event.target).entries());
+    console.log(fromData);
+})
