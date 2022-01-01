50d4f8ee1e3fc5de9e097e3c771369d88d4b4814
Edward Edgar Arthur <108295488+edgararthur@users.noreply.github.com>
2022-01-01 10:36:38 +0000

    handle http request to server

--- src/scripts/js/index.js
+++ src/scripts/js/index.js
@@ -6,65 +6,64 @@ const joined = document.querySelectorAll(".form");
 const joinedShow = document.querySelectorAll(".join p");
 const join__form = document.querySelector(".join__form");
 
-import axios from 'axios'
+const room = document.querySelector(".user__interest");
 
 // show home/profile menu
 toggleMenu.addEventListener("click", (event) => {
-	menu.classList.toggle("menu__user__show");
+    menu.classList.toggle("menu__user__show");
 });
 
 const sendHttpRequest = (method, url, data) => {
-	return fetch(url, {
-		method: method,
-		body: JSON.stringify(data),
-		headers: data ? {'Content-Type': 'application/json'} : {}
- 	}).then(response => {
-		return response.json();
-	})
-}
+    return fetch(url, {
+        method: method,
+        body: JSON.stringify(data),
+        headers: data ? { "Content-Type": "application/json" } : {},
+    }).then((response) => {
+        return response.json();
+    });
+};
 
 const getRooms = async (url) => {
-	try {
-		const response = await axios.get(url);
-		console.log(response.data)
-	} catch (error) {
-		console.log(error);
-	}
-}
+    try {
+        const response = await axios.get(url);
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+};
 
-getRooms('http://127.0.0.1:/api/v1/room/rooms')
+getRooms("http://127.0.0.1:8000/api/v1/room/rooms");
 
 // search for study rooms
 inputSearch.addEventListener("submit", (event) => {
-	event.preventDefault();
-	getResponse().then((contents) => {
-		contents.forEach((content) => {
-			const dataLowerCase = data["search"].toLowerCase();
-			const contentLowerCase = content["roomName"].toLowerCase();
-			if (dataLowerCase == contentLowerCase) return "found";
-			return "search not found";
-		});
-	});
-	inputSearch.reset();
-	return false;
+    event.preventDefault();
+    getResponse().then((contents) => {
+        contents.forEach((content) => {
+            const dataLowerCase = data["search"].toLowerCase();
+            const contentLowerCase = content["roomName"].toLowerCase();
+            if (dataLowerCase == contentLowerCase) return "found";
+            return "search not found";
+        });
+    });
+    inputSearch.reset();
+    return false;
 });
 
 // join a room and show if user has joined a room
 joined.forEach((join) => {
-	join.addEventListener("click", (event) => {
-		event.preventDefault();
-		event.target.parentElement.style.display = "none";
-		let joinedParagraph;
-		for (let paragraph in joinedShow) {
-			let promptParagraph = event.currentTarget.nextSibling.nextSibling;
-			if (promptParagraph == joinedShow[paragraph]) {
-				promptParagraph.style.visibility = "visible";
-				let interestData = event.currentTarget;
-			}
-			joinedParagraph = event.currentTarget;
-		}
-
-	});
+    join.addEventListener("click", (event) => {
+        event.preventDefault();
+        event.target.parentElement.style.display = "none";
+        let joinedParagraph;
+        for (let paragraph in joinedShow) {
+            let promptParagraph = event.currentTarget.nextSibling.nextSibling;
+            if (promptParagraph == joinedShow[paragraph]) {
+                promptParagraph.style.visibility = "visible";
+                let interestData = event.currentTarget;
+            }
+            joinedParagraph = event.currentTarget;
+        }
+    });
 });
 
-
+// open chat room
