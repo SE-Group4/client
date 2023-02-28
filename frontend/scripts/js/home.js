ebcde7ace618485bd892950c7987a610a5b89d96
edgararthur <gilfoyle.arthur@protonmail.com>
2023-02-28 16:38:01 +0000

    refactored the fetch function to make it a global variable

--- frontend/scripts/js/home.js
+++ frontend/scripts/js/home.js
@@ -6,31 +6,45 @@ const joined = document.querySelectorAll('.join form');
 const joinedShow = document.querySelector('.join__paragraph');
 const join__form = document.querySelector('.join__form');
 
+// fetch data
+const getData = fetch('/frontend/data/chatRooms.json');
+
 // show home/profile menu
 toggleMenu.addEventListener('click', (event) => {
 	menu.classList.toggle('menu__user__show');
 });
 
 // reading file
-fetch('/frontend/data/chatRooms.json')
-	.then((response) => response.json())
-	.then((contents) => {
-		// Do something with the contents of the file
-		console.log(contents);
-	})
-	.catch((error) => {
-		// Handle errors
-		console.error('Error loading file: ' + error);
-	});
+getData;
+// .then((response) => response.json())
+// .then((contents) => {
+// 	// Do something with the contents of the file
+// 	console.log(contents[1]);
+// })
+// .catch((error) => {
+// 	// Handle errors
+// 	console.error('Error loading file: ' + error);
+// });
 
 // search for study rooms
-// inputSearch.addEventListener('submit', (event) => {
-// 	event.preventDefault();
-// 	const data = Object.fromEntries(new FormData(event.target).entries());
-// 	console.log(data);
-// 	inputSearch.reset();
-// 	return false;
-// });
+inputSearch.addEventListener('submit', (event) => {
+	event.preventDefault();
+	const data = Object.fromEntries(new FormData(event.target).entries());
+	getData
+		.then((response) => response.json())
+		.then((contents) => {
+			contents.forEach((content) => {
+				if (
+					data['search'].toLowerCase() ==
+					content['roomName'].toLowerCase()
+				) {
+					console.log('found!!');
+				}
+			});
+		});
+	inputSearch.reset();
+	return false;
+});
 
 // join a room and show if user has joined a room
 // joined.forEach((join) => {
