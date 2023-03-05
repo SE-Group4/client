f057c4b1d491dc5714fa1a490376178f51d2355d
edgararthur <gilfoyle.arthur@protonmail.com>
2023-03-05 08:50:16 +0000

    scripts to make the menu profile interactive

--- frontend/scripts/js/home.js
+++ frontend/scripts/js/home.js
@@ -2,8 +2,8 @@
 const toggleMenu = document.querySelector('.fa-bars');
 const menu = document.querySelector('.menu__user');
 const inputSearch = document.querySelector('form');
-const joined = document.querySelectorAll('.join form');
-const joinedShow = document.querySelector('.join__paragraph');
+const joined = document.querySelectorAll('.form');
+const joinedShow = document.querySelectorAll('.join p');
 const join__form = document.querySelector('.join__form');
 
 // fetch data
@@ -39,6 +39,8 @@ inputSearch.addEventListener('submit', (event) => {
 					content['roomName'].toLowerCase()
 				) {
 					console.log('found!!');
+				} else {
+					console.log('search not found');
 				}
 			});
 		});
@@ -47,8 +49,40 @@ inputSearch.addEventListener('submit', (event) => {
 });
 
 // join a room and show if user has joined a room
-// joined.forEach((join) => {
-// 	join.addEventListener('click', (event) => {
-// 		console.log('clicked join');
-// 	});
-// });
+joined.forEach((join) => {
+	join.addEventListener('click', (event) => {
+		event.target.parentElement.style.visibility = 'hidden';
+		joinedShow.forEach((joinedParagraph) => {
+			joinedParagraph.style.visibility = 'visible';
+			for (let i = 0; i < joinedParagraph; i++) {
+				if (join[i] == event.currentTarget) {
+					joinedParagraph[i].style.visibility = 'hidden';
+				}
+			}
+		});
+		for (let i = 0; i < join.length; i++) {
+			if (join[i] !== event.target) {
+				join[i].style.visibility = 'visible';
+			}
+		}
+		// console.log('clicked');
+		// join.style.visibility = 'hidden';
+		// joinedShow.forEach((joinedParagraph) => {
+		// 	joinedParagraph.style.visibility = 'visible';
+		// });
+	});
+});
+
+document.body.addEventListener('click', function (event) {
+	// Check if a div element was clicked
+	if (event.target.classList.contains('myDiv')) {
+		// Show the clicked div element
+		event.target.style.display = 'block';
+		// Hide the other div elements
+		for (var i = 0; i < divs.length; i++) {
+			if (divs[i] !== event.target) {
+				divs[i].style.display = 'none';
+			}
+		}
+	}
+});
