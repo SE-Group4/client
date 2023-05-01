90864f61dbb970af60597aa6e3d1865b2acc89f4
Edward Edgar Arthur <108295488+edgararthur@users.noreply.github.com>
2023-05-01 13:54:27 +0000

    add regular expression to validate email

--- src/scripts/js/signup.js
+++ src/scripts/js/signup.js
@@ -1,4 +1,4 @@
-const form = document.querySelector('form');
+const form = document.querySelector("form");
 // const eye = document.querySelectorAll('.fa-eye')
 // const password = document.querySelector('#password')
 // const confirmPassword = document.querySelector('#confirm__password');
@@ -7,10 +7,10 @@ const form = document.querySelector('form');
 const confirmPasswordInput = (firstValue, lastValue) => {
     if (firstValue == lastValue) return true;
     return false;
-}
+};
 
 // get signup info data from user
-form.addEventListener('submit', (event) => {
+form.addEventListener("submit", (event) => {
     event.preventDefault();
     const formData = Object.fromEntries(new FormData(event.target).entries());
     let firstName = formData.first_name;
@@ -20,31 +20,40 @@ form.addEventListener('submit', (event) => {
     let confirmData = formData.confirm__password;
     console.log(formData);
     try {
-        if ((firstName != '' && lastName != '') && (confirmPasswordInput(passwordData, confirmData) && emailValidate(email))) {
-            window.location.href = "/frontend/pages/interests.html"
+        if (
+            firstName != "" &&
+            lastName != "" &&
+            confirmPasswordInput(passwordData, confirmData) &&
+            emailValidate(email)
+        ) {
+            window.location.href = "/src/pages/interests.html";
         }
     } catch (error) {
         return error;
     }
-    form.reset()
-    return false
-})
+    form.reset();
+    return false;
+});
 
 // validate email
 function emailValidate(email) {
-    let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    let regex =
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return regex.test(email);
 }
 
 // show password
-eye.forEach(eyecon => {
-    eyecon.addEventListener('click', e => {
-        if (e.target.parentElement.classList == 'password') {
-            password.type == 'password' ? password.type = 'text' : password.type = 'password'
+eye.forEach((eyecon) => {
+    eyecon.addEventListener("click", (e) => {
+        if (e.target.parentElement.classList == "password") {
+            password.type == "password"
+                ? (password.type = "text")
+                : (password.type = "password");
         }
-        if (e.target.parentElement.classList == 'confirm__password') {
-            confirmPassword.type == 'password' ? confirmPassword.type = 'text' : confirmPassword.type = 'password';
+        if (e.target.parentElement.classList == "confirm__password") {
+            confirmPassword.type == "password"
+                ? (confirmPassword.type = "text")
+                : (confirmPassword.type = "password");
         }
-
-    })
-})
+    });
+});
